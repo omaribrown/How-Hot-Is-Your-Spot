483c5eb569b02b5e3362dec0d1ac37e8029b71d5
Omari Brown <omaris.brown@gmail.com>
2020-07-28 15:56:28 -0400

    Created component

---
+++ hhiys/src/components/LocationSearch.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default class LocationSearch extends React.Component {
+    constructor() {
+        super()
+
+        async 
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
