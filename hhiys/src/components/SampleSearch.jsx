a873f5146fa0c5b4fffb25172a2ca9c63f299fc7
Omari Brown <omaris.brown@gmail.com>
2020-07-30 15:08:17 -0400

    built ontop of boilerplate code

---
+++ hhiys/src/components/SampleSearch.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+
+// https://www.youtube.com/watch?v=uJYqQdnw8LE
+
+export default function SampleSearch() {
+  const [address, setAddress] = React.useState("");
+
+  const [zipcode, setZipcode] = React.useState("");
+
+  const handleSelect = async (value) => {
+    const result = await geocodeByAddress(value);
+    setAddress(value);
+    const zipComp = await result[0].address_components[6].short_name;
+    setZipcode(zipComp);
+  };
+
+  return (
+    <div>
+      <PlacesAutocomplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <p>Zipcode: {zipcode}</p>
+
+            <input {...getInputProps({ placeholder: "Type address" })} />
+
+            <div>
+              {loading ? <div>...loading</div> : null}
+
+              {suggestions.map((suggestion) => {
+                const style = {
+                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff",
+                };
+
+                return (
+                  <div {...getSuggestionItemProps(suggestion, { style })}>
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    </div>
+  );
+}
