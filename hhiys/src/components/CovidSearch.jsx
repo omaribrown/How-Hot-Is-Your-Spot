9ee810b9c95670be83f169abc79b840604261688
Omari Brown <omaris.brown@gmail.com>
2020-07-31 20:46:45 -0400

    cleaning up code

--- hhiys/src/components/CovidSearch.jsx
+++ hhiys/src/components/CovidSearch.jsx
@@ -5,45 +5,32 @@ import Zipcode from './Context'
 // import ZipcodeContext from './SampleSearch'
 
 //cases per 1k people
-const zipParam = 30316;
-const call = `https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=30316&daysInPast=1`
+
 
 export default function CovidSearch() {
     const data = useContext(Zipcode)
+    // const zipParam = 30316;
+    // const call = `https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=30127&daysInPast=1`
 
     console.log(data)
     console.log(data.zipcode)
-    // static contextType = ZipcodeContext
 
-    // componentDidMount() {
-    //     const passedObj = this.context
-    //     console.log(passedObj)
-    // }
-    // constructor() {
-    //     super()
-
-    //     this.state = {
-    //         info: [],
-    //     }
-    // }
-
-    // async getCases() {
-    //     try {
-    //         const result = await axios.get(call)
-    //         this.setState({ info: result.data })
-    //         console.log(this.state.info)
-    //     } catch {
-    //         console.error("something ain't right")
-    //     }
-    // }
-    
-    // componentDidMount() {
-    //     this.getCases()
-    //     console.log(this.state.info)
-    // }
+    async function handleZipcode() {
+        try {
+        const result = await axios.get(`https://localcoviddata.com/covid19/v1/cases/newYorkTimes?zipCode=${data.zipcode}&daysInPast=1`)
+        console.log(result.data)
+        const positiveCt = await result.counties[0].historicData[0].positiveCt
+        console.log(positiveCt)
+    } catch {
+        console.error("something is wrong in covid search")
+    }
+}
 
         return (
             <div>
+                <button onClick={handleZipcode}>
+                    Click me to do something 
+                </button>
                 <h1>CovidSearch</h1>
             </div>
         )
