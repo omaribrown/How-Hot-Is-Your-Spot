1236aab453d344842a1b1c9995928535b09ebe5b
Omari Brown <omaris.brown@gmail.com>
2020-08-02 22:36:17 -0400

    created footer

---
+++ hhiys/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Styles/Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer-bar'>
+            footer content content content
+        </div>
+    )
+}
+
+export default Footer
