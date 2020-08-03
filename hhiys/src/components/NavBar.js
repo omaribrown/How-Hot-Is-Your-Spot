ef791dcaee1892a9eb743ed9fcdd07df94915430
Omari Brown <omaris.brown@gmail.com>
2020-08-02 22:59:08 -0400

    started working on nav

--- hhiys/src/components/NavBar.js
+++ hhiys/src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from 'react'
 function NavBar() {
     return (
         <div>
-            
+            <ul>
+                <li>See Cases Around Restaurants</li>
+                <li>See cases Around Your Zipcode</li>
+                <li>Resources</li>
+            </ul>
         </div>
     )
 }
