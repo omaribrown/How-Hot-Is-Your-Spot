0c5fb858c8c07f695212e61c96c2454fe23923d2
Omari Brown <omaris.brown@gmail.com>
2020-08-02 22:36:00 -0400

    testing

--- hhiys/src/App.js
+++ hhiys/src/App.js
@@ -3,20 +3,19 @@ import './App.css';
 import SampleSearch from './components/SampleSearch'
 import CovidSearch from './components/CovidSearch'
 import Context from './components/Context'
-// import { ZipcodeProvider } from './components/SampleSearch'
-// import LocationSearch from './components/LocationSearch'
-// import SampleSearch from './components/SampleSearch'
-// import CovidSearch from './components/CovidSearch'
+import Footer from './components/Footer'
 
 export default function App() {
   const [zipcode, setZipcode] = React.useState("");
 
   return (
     <div className="App">
+      
       <Context.Provider value={{ zipcode, setZipcode }}>
         <SampleSearch />
         <CovidSearch />
       </Context.Provider>
+      <Footer />
     </div>
   );
 }
