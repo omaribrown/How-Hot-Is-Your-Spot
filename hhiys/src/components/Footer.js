6118aaaab2a3a562a50e4752a77b1001789eabf9
Omari Brown <omaris.brown@gmail.com>
2020-08-04 10:22:38 -0400

    Update Footer.js

--- hhiys/src/components/Footer.js
+++ hhiys/src/components/Footer.js
@@ -4,7 +4,8 @@ import './Styles/Footer.css'
 function Footer() {
     return (
         <div className='footer-bar'>
-            footer content content content
+            <h4>For location data, this website uses the Google Places Autocomplete API and can be found <a href='https://developers.google.com/places/web-service/autocomplete'>here.</a></h4>
+            <h4>For Covid-19 data, this website uses data collected by The New York Times and you may access this data <a href='https://anypoint.mulesoft.com/exchange/68ef9520-24e9-4cf2-b2f5-620025690913/covid19-data-tracking-api/minor/1.0/pages/home/'>here.</a></h4>
         </div>
     )
 }
