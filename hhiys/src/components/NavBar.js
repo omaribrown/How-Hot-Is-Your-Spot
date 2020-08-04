6acba28321531062e1f71aa36f6e208a5169f213
Omari Brown <omaris.brown@gmail.com>
2020-08-04 01:47:05 -0400

    added router

--- hhiys/src/components/NavBar.js
+++ hhiys/src/components/NavBar.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SampleSearch from './SampleSearch';
 
-function NavBar() {
-    return (
-        <div>
-            <ul>
-                <li>See Cases Around Restaurants</li>
-                <li>See cases Around Your Zipcode</li>
-                <li>Resources</li>
-            </ul>
-        </div>
-    )
+export default function NavBar() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/restaurant-search">See Cases Around Restaurants</Link>
+            </li>
+            <li>
+              <Link to="/zipcode-search">See cases Around Your Zipcode</Link>
+            </li>
+            <li>
+              <Link to="/resources">Resources</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/restaurant-search">
+            <SampleSearch />
+          </Route>
+          <Route path="/zipcode-search">
+            {alert("This page is still under construction. You are being redirected to the homepage.")}
+            <SampleSearch />
+          </Route>
+          <Route path="/">
+            {alert("This page is still under construction. You are being redirected to the homepage.")}
+            <SampleSearch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
-
-export default NavBar
