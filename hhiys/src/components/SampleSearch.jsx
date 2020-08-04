c1a95435e2336f32c211276bd0ea988c5818e404
Omari Brown <omaris.brown@gmail.com>
2020-08-04 11:36:15 -0400

    Update SampleSearch.jsx

--- hhiys/src/components/SampleSearch.jsx
+++ hhiys/src/components/SampleSearch.jsx
@@ -15,8 +15,14 @@ export default function SampleSearch() {
 
     const handleSelect = async (value) => {
         const result = await geocodeByAddress(value);
+        console.log(result)
         setAddress(value);
-        const zipComp = await result[0].address_components[6].short_name;
+        let zipComp = await result[0].address_components[6].short_name;
+        // FIXME this is the check for if we're taking the zipcode from the right index
+        if (isNaN(zipComp)) {
+          let zipComp = await result[0].address_components[7].short_name;
+          console.log('new zipcomp is ', zipComp)
+        }
         console.log(zipComp)
         setZipcode(zipComp);
   };
@@ -34,7 +40,9 @@ export default function SampleSearch() {
       <div className='bio-container'>
         <div className='search-bio'>
           <p>This is an application that lets you enter your favorite restaurant and see the number of Covid-19 cases for that zip code in comparison to neighboring zip codes, the state, and the national averages. This app allows the user to see how effected their favorite areas to go are.</p> 
-          <h5>Known Bugs: If the Zipcode box populates with a state code or country code instead of a zipcode, you'll need to refresh and try another restaurant. This issue comes from when the application is trying to grab the zipcode from the Google Places API and occasionally grabs the wrong index of data.</h5>
+          <h4>Known Bugs: If the Zipcode box populates with a state code or country code instead of a zipcode, you'll need to refresh and try another restaurant. </h4>
+          <h5>This issue comes from when the application is trying to grab the zipcode from the Google Places API and occasionally grabs the wrong index of data.</h5>
+          <h5>For whatever reason, my API from The New York Times doesnt have Covid-19 case data from New York. So a few suggestions from Atlanta (where I live) are "Bad Daddy's Burger Bar", "The Varsity", and </h5>
         </div>
       </div>
       <PlacesAutocomplete
