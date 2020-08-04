e36d37f1f6f21eb711b3de1f1cb2983419d5d598
Omari Brown <omaris.brown@gmail.com>
2020-08-04 02:39:42 -0400

    Update CovidSearch.jsx

--- hhiys/src/components/CovidSearch.jsx
+++ hhiys/src/components/CovidSearch.jsx
@@ -31,7 +31,7 @@ export default function CovidSearch() {
 
         return (
             <div className='main-div'>
-                <h3>Next, hit the button below!</h3>
+                {/* <h3>Next, hit the button below!</h3> */}
                 <button onClick={handleZipcode}>
                     Display Nearby Covid-19 Data
                 </button>
