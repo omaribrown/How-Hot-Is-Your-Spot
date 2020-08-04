aaa4d7faa85bb0f1bee9b2070893d2397becad7c
Omari Brown <omaris.brown@gmail.com>
2020-08-04 02:39:21 -0400

    Update App.js

--- hhiys/src/App.js
+++ hhiys/src/App.js
@@ -4,15 +4,17 @@ import SampleSearch from './components/SampleSearch'
 import CovidSearch from './components/CovidSearch'
 import Context from './components/Context'
 import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 
 export default function App() {
   const [zipcode, setZipcode] = React.useState("");
 
   return (
     <div className="App">
-      
       <Context.Provider value={{ zipcode, setZipcode }}>
-        <SampleSearch />
+        <NavBar />  
+
+        {/* <SampleSearch /> */}
         <CovidSearch />
       </Context.Provider>
       <Footer />
